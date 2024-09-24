eddbba2eb99c81dc3a2a92aeafe30cc89a66cd9d
henry-schober <henry.schober@uconn.edu>
2024-09-24 15:40:44 -0400

    added script to automate creating cgs and pheno files

---
+++ Bruna_paper_CA_NE/CDCB Scripts/auto_script.sh
@@ -0,0 +1,33 @@
+#The purpose of this script is to run all the phenotype scripts
+cd /data/henry/henrys/first_lac/new_england/milk
+
+sh combined_ne_w_var_milk.sh
+
+cp cgs_30 ../.
+cp phenotypes_ne_no_small_first_lac ../.
+
+cd /data/henry/henrys/first_lac/california/milk
+
+sh combined_ca_w_var_milk.sh
+
+cp cgs_30 ../.
+cp phenotypes_ca_no_small_first_lac ../.
+
+
+cd /data/henry/henrys/first_lac/combined/ne
+
+sh ssd_phen_ne_1_10.sh
+
+cp phen_ne.clean* ../.
+
+cd /data/henry/henrys/first_lac/combined/ca
+
+sh ssd_phen_ca_1_10.sh
+
+cp phen_ca.clean* ../.
+
+cd /data/henry/henrys/first_lac/combined
+
+sh combining_phen.sh
+
+#stop here before going into two trait folder as we can use the variances found in the previous analysis
