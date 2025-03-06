d8c113e762137aecc798b8352e1b7519c92c8983
fragomen <fragomen@users.noreply.github.com>
2025-03-06 12:30:10 -0500

    Update renumf_famous_bulls.sh

--- TX_WI_analysis/test_days/famous_bulls/renumf_famous_bulls.sh
+++ TX_WI_analysis/test_days/famous_bulls/renumf_famous_bulls.sh
@@ -22,6 +22,17 @@ for x in $state
 
         sort +1 -2 renadd04.ped > sorted_renadd    
         join -1 1 -2 2 bulls_xref sorted_renadd > daughter_renadd04.ped
+
+        #the next step is to use the daughter ids from the file above and select their records:
+        awk '{print $10}' daughter_renadd04.ped | sort +0 -1 > famous_daughters_id #single column with the daughter original ids
+
+        #you can sort your original data file by the animal id column, and join with the id of the cows 
+        sort +(column -1)  -(column) original_data > sorted_data #where column is the column with animal ids (12 If I remember it right)
+
+        join -1 1 -2 column famous_daughters_id sorted_data > famous_daughters.data
+        #your new joint file will have different columns, since column 1 will be the id
+
+        #run renumf90 again, using the reduced phen file and the original pedigree file that you used before
         
         #here we wanna join the bulls_xref (file containing bull id in renadd format that have more than 100 daughters and then join it by column $2 in renadd as that is the father column) 
 
